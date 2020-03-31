770aa2c92e11e37c79341e15448976d1ae8805e8
Krishna Sharma <sharmakrishna9501@gmail.com>
2020-03-31 12:14:50 +0000

    Remove a Progression done

---
+++ Remove_a_progression.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+
+    long long n , x;
+    for(int i = 0; i<t; i++){
+        cin>>n>>x;
+        cout<<2 * x<<endl;
+    }
+    return 0;
+}
