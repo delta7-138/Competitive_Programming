cdb42943c54640dc98e6efed9533bddf16582691
delta7-138 <krishna.sharma@iiitb.org>
2020-07-30 20:46:46 +0530

    adding temp

---
+++ Required_Remainder_1374A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+ll largest_rem(ll x , ll y , ll n){
+    ll k = 0;
+
+    ll quo = n/y;
+    if(n%y==0){
+        return n;
+    }
+
+    while(quo*x + y>n){
+        quo = quo-1;
+        if(quo*x + y <n){
+            return (quo *x + y);
+        }
+    }
+    return 0;
+}
+
+int main(){
+    int t;
+    cin>>t;
+
+    ll x , y , n;
+    vector <int>ans;
+    for(int i = 0; i<t; i++){
+        cin>>x>>y>>n;
+        ans.push_back(largest_rem(x , y , n));
+    }
+    
+    for(int i = 0; i<t; i++){
+        cout<<ans[i]<<endl;
+    }
+    return 0;
+}
